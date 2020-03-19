14addde772549bbe42c98452d38e178dfe605ca2
Evgenii Nazarov <ev66647260@gmail.com>
2020-03-18 23:03:14 -0700

    added update action

--- src/App.js
+++ src/App.js
@@ -21,42 +21,42 @@ function App(props) {
 <UserLoginForm/>
 
 
-            {/*<h2>Counters</h2>*/}
-            {/*Total {props.myCounters.reduce((acc, el) => acc + el.counterValue, 0)}*/}
-            {/*<hr/>*/}
+            <h2>Counters</h2>
+            Total {props.myCounters.reduce((acc, el) => acc + el.counterValue, 0)}
+            <hr/>
 
-            {/*{props.myCounters.map((el) => (*/}
-            {/*    <Counter key={el.id}*/}
-            {/*             name={el.counterName}*/}
-            {/*             id={el.id}*/}
-            {/*             value={el.counterValue}*/}
-            {/*             counterPlus={props.myCounterPlus}*/}
-            {/*             counterMinus={props.myCounterMinus}*/}
-            {/*             counterDelete={props.deleteCounter}*/}
-            {/*             counterUpdate={props.counterUpdate}*/}
-            {/*    />*/}
-            {/*))}*/}
-            {/*<button onClick={props.myReset}>RESET</button>*/}
-            {/*<hr/>*/}
+            {props.myCounters.map((el) => (
+                <Counter key={el.id}
+                         name={el.counterName}
+                         id={el.id}
+                         value={el.counterValue}
+                         counterPlus={props.myCounterPlus}
+                         counterMinus={props.myCounterMinus}
+                         counterDelete={props.deleteCounter}
+                         counterUpdate={props.counterUpdate}
+                />
+            ))}
+            <button onClick={props.myReset}>RESET</button>
+            <hr/>
 
-            {/*<AddCounter onSubmit={addCounterButtonHandler}*/}
-            {/*/>*/}
+            <AddCounter onSubmit={addCounterButtonHandler}
+            />
 
         </div>
     );
 }
 
 const mapStateToProps = state => ({
-    // myCounters: state.counters
+    myCounters: state.counters
 });
 
 const mapDispatchToProps = dispatch => ({
-    // myCounterPlus: (id) => dispatch(counterPlus(id)),
-    // myCounterMinus: (id) => dispatch(counterMinus(id)),
-    // myReset: () => dispatch(countersReset()),
-    // addCounter: (counterName, counterValue) => dispatch(countersAdd(counterName, counterValue)),
-    // deleteCounter: (id) => dispatch(counterDelete(id)),
-    // counterUpdate: (counterName,id) => dispatch(counterUpdate(counterName,id))
+    myCounterPlus: (id) => dispatch(counterPlus(id)),
+    myCounterMinus: (id) => dispatch(counterMinus(id)),
+    myReset: () => dispatch(countersReset()),
+    addCounter: (counterName, counterValue) => dispatch(countersAdd(counterName, counterValue)),
+    deleteCounter: (id) => dispatch(counterDelete(id)),
+    counterUpdate: (counterName,id) => dispatch(counterUpdate(counterName,id))
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
